cfdcce868ee27ea1a528439ec86aa711c4877a24
albaraa417 <albaraa417@gmail.com>
2025-07-13 14:18:41 +0300

    auth

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -9,6 +9,7 @@
     <router-link class=" btn btn-primary" to="/">Home</router-link>
     <router-link class=" btn btn-primary" to="/about">About</router-link>
     <router-link class=" btn btn-primary" to="/contact">Contact</router-link>
+    <router-link class=" btn btn-primary" to="/Authentication">login</router-link>
 </nav>
 </header>
 
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -3,7 +3,29 @@ import { useRouter } from 'vue-router'
 </script>
 
 <template>
-  
+  <section>
+      <div class="row">
+        <!-- left page Moon background photo-->
+        <div class="col-6">
+          <img src="../assets/Auth-background.png" alt="Moon Background" class="img-fluid">
+        </div>
+
+        <!-- Right page welcoming-->
+        <div class="col-6">
+          <div class="container text-center justify-content-center align-items-center">
+            <h3>Welcome Back!</h3>
+            <h5>Enter you Credentials to access your account</h5>
+
+            <!-- Login From -->
+
+            <form class="mt-4">
+              <label for="email-input" class="form-label">Email</label>
+              <input type="email" id="email-input" name="email" class="form-control">
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
 </template>
 
 <style scoped>
