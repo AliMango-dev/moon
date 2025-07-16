48639715d14b6a733e1b0a769a319e54291cecd4
AliMango-dev <romario790.aa@gmail.com>
2025-07-16 10:25:58 +0300

    footer and about

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -72,12 +72,108 @@ const shouldHideLayout = computed(() => hideLayoutRoutes.includes(route.name))
     <slot />
 </main>
 
-<footer>
+<footer style="background-color: #3A3845; color: #CAC9CF;">
+    <!-- Grid container -->
+    <div class="container p-4" style="width: 1110px;">
+      <!--Grid row-->
+      <div class="row justify-content-center">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0 " style="border-right: 1px solid #CAC9CF; height: 318px; margin-top: 48px;">
+          <img src="../assets/logo.svg">
 
+          <p style="color: #E5E5E5; margin-top: 24px;">
+            Lorem ipsum dolor sit amet consectetur <br> adipiscing elit aliquam mauris sed ma
+          </p>
+
+          <button class="btn btn-outline-light" style="width: 142px; height: 41px;">Get Started<img src="../assets/contact-arrow-icon.svg"></button>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0" style="margin-top: 48px;">
+          <h5 class="text-uppercase">About us</h5>
+
+          <ul class="list-unstyled mb-0 mt-3">
+            <li>
+              <router-link class="nav-link">Mission</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Our team</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Awards</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Testimonials</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Privacy policy</router-link>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0" style="margin-top: 48px;">
+          <h5 class="text-uppercase">Services</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <router-link class="nav-link">Web design</router-link>
+            </li>
+            <li>
+              <router-link  class="nav-link">Web development</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Mobile design</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">UI/UX design</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Branding design</router-link>
+            </li>
+          </ul>
+        </div>
+
+        <div class="col-lg-2 col-md-6 mb-4 mb-md-0" style="margin-top: 48px;">
+          <h5 class="text-uppercase">Portfolio</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <router-link  class="nav-link">Corporate websites</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">E-commerce</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Mobile apps</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">Landing pages</router-link>
+            </li>
+            <li>
+              <router-link class="nav-link">UI/UX projects</router-link>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class=" text-center p-3 d-flex justify-content-center" style="border-top: 1px solid #CAC9CF; width: 1110px; margin-left: auto; margin-right: auto;">
+      Copyright © 2023 Moon| All Rights Reserved | <router-link class="nav-link mx-1"><span style="text-decoration: underline;">Terms and Conditions</span></router-link>  | <router-link class="nav-link mx-1"><span style="text-decoration: underline;">Privacy policy</span></router-link> 
+    </div>
+    <!-- Copyright -->
 </footer>
 
 </template>
 
 <style scoped>
-
+li{
+  margin: 24px;
+}
 </style>
